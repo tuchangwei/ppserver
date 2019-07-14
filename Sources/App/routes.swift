8352b113bba6cb607b8338bafc956d593d0e4b36
Changwei Tu <changweitu@gmail.com>
2019-07-14 12:26:05 +0800

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,13 +8,25 @@ public func routes(_ router: Router) throws {
     }
     
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get("arts") { req -> [Art] in
+        let baseURL = "http://\(NIOServerConfig.default().hostname):\(NIOServerConfig.default().port)/"
+        return [
+            Art(url: baseURL + "diamond.png"),
+            Art(url: baseURL + "flame.png"),
+            Art(url: baseURL + "girl.png"),
+            Art(url: baseURL + "heart.png"),
+            Art(url: baseURL + "marked_man.png"),
+            Art(url: baseURL + "rocket.png"),
+            Art(url: baseURL + "sword.png"),
+            Art(url: baseURL + "toy.png"),
+        ]
     }
-
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+struct Art: Content {
+    var url: String
 }
